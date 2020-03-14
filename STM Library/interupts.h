ea6c7ee0b9371d7cf4105a217ae2c3b8b65ed273
Avery <averycameron35@gmail.com>
2020-03-13 18:11:15 -0600

    Updated Interupt file

--- STM Library/interupts.h
+++ STM Library/interupts.h
@@ -5,13 +5,22 @@
  * Authors: Avery Cameron
  *
  *****************************************************************************/
- 
- void enableAFIO(void);
- void enableExti0PA(void);
- void unmaskExti0(void);
- void fallingEdgeExti0(void);
- void unmaskNvicExti0(void);
- 
+ #include "stm32f10x.h"
+ /*
+ * Name: setupExtiInterupt
+ * Inputs: 
+ * 		exit: EXTI Interupt to configure, PX0=0, PX1=1, PX2=2 etc.
+ *		pinGroup: Pin Group to configure, PA=0, PB=1, PC=2
+ *		edgeDetection: Determines interupt trigger, falling or rising edge
+ * 		nvicMask: NVIC entry to unmask (6 for EXTI0)
+ * Return: None
+ * Description:
+ * Sets up an EXTI Interupt for the given pin number and pin group
+ * Allows user to configure the interupt trigger and unmask the interupt
+ */
+ void setupExtiInterupt(uint8_t exti, uint8_t pinGroup, uint16_t edgeDetection, uint16_t nvicMask);
  
  void setupSysTick(void);
  void setupExti0PA(void);
+ void setupExti2PA(void);
+ void setupExti3PA(void);
