854e469fa251deb48d506587f2a2e82a01d69c7e
Avery <averycameron35@gmail.com>
2020-03-19 15:13:07 -0600

    Updated file formatting

--- STM Library/Servo.c
+++ STM Library/Servo.c
@@ -1,63 +1,61 @@
- #include "clock.h"
- #include "gpio.h"
- #include "lcd.h"
- #include "pwm.h"
- #include "interrupts.h"
- uint8_t count = 0;
- uint8_t count2 = 0;
- int time = 0;
- uint8_t edgeDetector = 1;
- int main(){
-	 ClockInit();
-	 GpioClockInit();
-	 ConfigureLeds();
-	 led_IO_init();
-	 LcdInit();
-	 GPIOC->CRH &= 0xFFF44444;
-	 GPIOA->CRL &= 0xFFFF00FF;
-	 GPIOA->CRL |= 0x00003400;
-	 uint16_t period = 400;
-	 uint16_t pulsewidth = 15;
-	 PwmInit(period, pulsewidth);
-	 //setup systick interupt
-	 setupSysTick();
-	 //Setup interupt on Pin A3 Rising Edge
-	 setupExtiInterrupt(3, 0, NVIC_ISER_SETENA_9);
-	 uint8_t counted = 0;
-	 while (1){
-		 uint8_t ir1 = ReadIR(1);
-		 uint8_t ir2 = ReadIR(2);
-		 uint8_t ir3 = ReadIR(3);
-		 uint8_t ir4 = ReadIR(4);
-		 uint8_t ir5 = ReadIR(5);
-		 pulsewidth = 15;
-		 setDutyCycle(pulsewidth);
-		 Delay(5000000);		 
-		 pulsewidth = 30;
-		 setDutyCycle(pulsewidth);
-		 Delay(5000000);
-		 pulsewidth = 45;
-		 setDutyCycle(pulsewidth);
-		 Delay(5000000);
-		 //UltraSonic Sensor Code 
-	   
-		 GPIOA->BSRR = 0x00040000;
-		 Delay(30);
-		 GPIOA->BSRR |= 0x00000004;
-		 Delay(60);
-		 GPIOA->BSRR = 0x00040000;
-		 LcdFirstLine();
-		 reg_out(time);
-		 LcdSecondLine();
-		 reg_out(time);
-	 }
-	 return 0;
- }
- void SysTick_Handler(){
-		GPIOC->ODR ^= GPIO_ODR_ODR8;
- }
- 
-  void EXTI3_IRQHandler(){
-	 EXTI->PR |= EXTI_PR_PR3;
-	 edgeDetector ^= edgeDetector;
- }
+#include "clock.h"
+#include "gpio.h"
+#include "lcd.h"
+#include "pwm.h"
+#include "interrupts.h"
+uint8_t count = 0;
+int time = 0;
+int main(){
+	ClockInit();
+	GpioClockInit();
+	ConfigureLeds();
+	led_IO_init();
+	LcdInit();
+	/*
+	GPIOC->CRH &= 0xFFF44444;
+	GPIOA->CRL &= 0xFFFF00FF;
+	GPIOA->CRL |= 0x00003400;
+	uint16_t period = 400;
+	uint16_t pulsewidth = 15;
+	PwmInit(period, pulsewidth);
+	//setup systick interupt
+	*/
+	setupSysTick();
+	//Setup interupt on Pin A3 Rising Edge
+	setupExtiInterrupt(3, 0, NVIC_ISER_SETENA_9);
+	while (1){
+		/*
+		uint8_t ir1 = ReadIR(1);
+		uint8_t ir2 = ReadIR(2);
+		uint8_t ir3 = ReadIR(3);
+		uint8_t ir4 = ReadIR(4);
+		uint8_t ir5 = ReadIR(5);
+		pulsewidth = 15;
+		setDutyCycle(pulsewidth);
+		Delay(5000000);		 
+		pulsewidth = 30;
+		setDutyCycle(pulsewidth);
+		Delay(5000000);
+		pulsewidth = 45;
+		setDutyCycle(pulsewidth);
+		Delay(5000000);
+		//UltraSonic Sensor Code 
+	   */
+		GPIOA->ODR &=  0xFFFFFFFB;
+		Delay(600000);
+		GPIOA->ODR |= 0x00000004;
+		Delay(600000);
+		GPIOA->ODR &=  0xFFFFFFFB;
+		LcdFirstLine();
+		reg_out(time);
+		LcdSecondLine();
+		reg_out(time);
+	}
+}
+void SysTick_Handler(){
+	GPIOC->ODR ^= GPIO_ODR_ODR8;
+}
+
+void EXTI3_IRQHandler(){
+	EXTI->PR |= EXTI_PR_PR3;
+}
--- STM Library/gpio.h
+++ STM Library/gpio.h
@@ -1,56 +1,33 @@
 /******************************************************************************
- * Name:    gpio.h
- * Description: STM32 gpio functions
- * Version: V1.00
- * Authors: Avery Cameron
- *
- *****************************************************************************/
- #include "stm32f10x.h"
+* Name:    gpio.h
+* Description: STM32 gpio functions
+* Version: V1.00
+* Authors: Avery Cameron
+*
+*****************************************************************************/
+#include "stm32f10x.h"
+
+//initializes clocks for gpio a,b,c
+void GpioClockInit(void);
+void led_IO_init (void);
+//configures the led pins for output
+void ConfigureLeds(void);
  
- //initializes clocks for gpio a,b,c
- void GpioClockInit(void);
-  void led_IO_init (void);
- //configures the led pins for output
- void ConfigureLeds(void);
+//Updates leds based on given value
+void UpdateLeds(uint16_t);
+//Blinks all 4 leds
+void Blink(void);
  
- //reads the total dipswitch value
- uint16_t ReadDipSwitch(void);
- //reads the first position of the dipswitch
- uint16_t ReadDipSwitchPos1(void);
- //reads the second position of the dipswitch
- uint16_t ReadDipSwitchPos2(void);
- //reads the third position of the dipswitch
- uint16_t ReadDipSwitchPos3(void);
- //reads the fourth position of the dipswitch
- uint16_t ReadDipSwitchPos4(void);
- 
- //reads the red button value
- uint16_t ReadRedButton(void);
- //reads the black button value
- uint16_t ReadBlackButton(void);
- //reads the black button value
- uint16_t ReadBlueButton(void);
- //reads the black button value
- uint16_t ReadGreenButton(void);
- 
- //Updates leds based on given value
- void UpdateLeds(uint16_t);
- //Buttons update the leds directly
- void ButtonsUpdateLeds(void);
- //Blinks all 4 leds
- void Blink(void);
- 
- /**
- * Name: ReadIR
- * Input: IR Sensor to detect
- * Output: IR Sensor Output
- *
- * Input is a number from 1 to 5 
- * for the 5 IR Sensors on the board from
- * Pins C8 to C12
- *
- * Output is a 1 (no detection) 
- * or a 0 (detection, non black object within range)
- */
- uint8_t ReadIR(uint8_t);
- 
+/**
+* Name: ReadIR
+* Input: IR Sensor to detect
+* Output: IR Sensor Output
+*
+* Input is a number from 1 to 5 
+* for the 5 IR Sensors on the board from
+* Pins C8 to C12
+*
+* Output is a 1 (no detection) 
+* or a 0 (detection, non black object within range)
+*/
+uint8_t ReadIR(uint8_t);
