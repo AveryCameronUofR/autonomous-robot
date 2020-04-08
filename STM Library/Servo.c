33a6533b36297c7359d804dcfb77c4c6ea34aca7
Avery <averycameron35@gmail.com>
2020-04-07 22:28:38 -0600

    Added IR Line counting

--- STM Library/Servo.c
+++ STM Library/Servo.c
@@ -36,9 +36,58 @@ int main(){
 	Tim4PwmInit(100, motorPulseWidth);
 	ConfigureMotorInputs();
 	
+	// IR sensing code
+	uint32_t threshold_to_count = 12500;
+	uint32_t threshold_to_display = 25000;
+	OutputRegisterValue(threshold_to_display);
+	// the current number of navigation strips counted
+	uint8_t strip_count = 0;
+	
+	// how long we've been on a black or white surface
+	uint16_t ir_black_count = 0;
+	uint16_t ir_white_count = 0;
+	
+	uint8_t last_ir1 = 0;
+	uint8_t curr_ir1 = 0;
+	
+	bool counted_strip = false;
+	bool should_display = false;
+
 	while (1){
+		potentiometer = ConvertAdcChannel(2);
+		motorPulseWidth = convert_motor_speed(potentiometer);
 		MoveForward();
-		uint8_t ir1 = ReadIR(1);
+		curr_ir1 = ReadIR(1);
+		if (ir_white_count > threshold_to_display && should_display == true) 
+		{
+			// this is where to display to lcd
+			LcdFirstLine();
+			OutputRegisterValue(strip_count);
+			ir_black_count = 0;
+			ir_white_count = 0;
+			should_display = false;
+			strip_count = false;
+		} else {
+			if (curr_ir1 == last_ir1) 
+			{
+				// update the right counter
+				curr_ir1 == 1 ? ir_black_count++ : ir_white_count++;
+				
+				if (ir_black_count >= threshold_to_count && !counted_strip) 
+				{
+					counted_strip =  true;
+					should_display = true;
+					ir_black_count = 0;
+					strip_count++;
+				}
+			} else 
+			{
+				ir_black_count = 0;
+				ir_white_count = 0;
+				counted_strip = false;
+			}
+		}
+		last_ir1 = curr_ir1;
 		uint8_t ir2 = ReadIR(2);
 		uint8_t ir3 = ReadIR(5);
 		
@@ -46,22 +95,18 @@ int main(){
 			TurnLeft();
 			Delay(2500000);
 		}
-		LcdFirstLine();
-		potentiometer = ConvertAdcChannel(2);
-		motorPulseWidth = convert_motor_speed(potentiometer);
-		SetTim4DutyCycle(motorPulseWidth);
 		
-		OutputRegisterValue(motorPulseWidth);
+		motorPulseWidth = convert_motor_speed(0);
+		SetTim4DutyCycle(motorPulseWidth);
 		pulsewidth = 15;
 		SetTim1DutyCycle(pulsewidth);
-		Delay(5000000);
+		Delay(7000000);		 
 		pulsewidth = 30;
 		SetTim1DutyCycle(pulsewidth);
-		Delay(5000000);
+		Delay(7000000);
 		pulsewidth = 45;
 		SetTim1DutyCycle(pulsewidth);
-		Delay(5000000);
-		
+		Delay(8000000);
 		//UltraSonic Sensor Code 
 	  /*
 		GPIOA->ODR &= 0xFFFFFFFB;
